24d93dda984e4965f9b86b24822511d1b279186a
unknown <iahmed3000@gmail.com>
2018-05-01 15:56:18 +0300

    add files

---
+++ Mutamayizoon/app/src/main/java/com/example/android/mutamayizoon/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.android.mutamayizoon;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void departLink(View view) {
+        // Do something in response to button
+        String url = "http://forandroid3000.esy.es/mutamayizoon_go.php";
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.setPackage("com.android.chrome");
+        try {
+            startActivity(i);
+        } catch (ActivityNotFoundException e) {
+            // Chrome is probably not installed
+            // Try with the default browser
+            i.setPackage(null);
+            startActivity(i);
+        }
+    }
+
+    public void returnLink(View view) {
+        // Do something in response to button
+        String url = "http://forandroid3000.esy.es/mutamayizoon_return.php";
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.setPackage("com.android.chrome");
+        try {
+            startActivity(i);
+        } catch (ActivityNotFoundException e) {
+            // Chrome is probably not installed
+            // Try with the default browser
+            i.setPackage(null);
+            startActivity(i);
+        }
+    }
+}
